0390617c1164183986ec324637ca6011e31a13f8
Kai C <clausenkai2@gmail.com>
2024-02-08 15:02:31 -0800

    Add myfirstprogram.cpp exercise source.

---
+++ exercises/myfirstprogram.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+int main(void)
+{
+    int num; 
+
+    std::cout << "Enter your favorite number: " << std::endl;
+
+    std::cin >> num;
+
+    std::cout <<"My favorite number is " << num << " too!" << std::endl;
+
+    return 0;
+}
